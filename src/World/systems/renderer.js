95002841d4b14872bb1941b7ecd1914cc0fbfc2c
Lifeishoney <107403681+Lifeishoney@users.noreply.github.com>
2022-06-13 13:50:06 +0300

    Add files via upload

---
+++ src/World/systems/renderer.js
@@ -0,0 +1,12 @@
+
+import { WebGLRenderer } from '/vendor/three/build/three.module.js';
+
+function createRenderer() {
+  const renderer = new WebGLRenderer({ antialias: true });
+
+  renderer.physicallyCorrectLights = true;
+
+  return renderer;
+}
+
+export { createRenderer };
