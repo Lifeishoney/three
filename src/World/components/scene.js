ad63c37386e9a8b1929bc13fe01e919b000c3ca7
Lifeishoney <107403681+Lifeishoney@users.noreply.github.com>
2022-06-13 13:57:06 +0300

    Update scene.js

--- src/World/components/scene.js
+++ src/World/components/scene.js
@@ -1,4 +1,4 @@
-import { Color, Scene } from '/vendor/three/build/three.module.js'
+import { Color, Scene } from './vendor/three/build/three.module.js'
 
 function createScene() {
   const scene = new Scene();
